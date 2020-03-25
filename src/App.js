d86da421e18660e8359bfd241489f72d26982f0f
Anand Gautam <Anand.Gautam@cognizant.com>
2020-03-25 17:50:50 +0530

    finishing the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import Unsplash from '../src/api/Unsplash';
+
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 class App extends React.Component {
+
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await Unsplash
+      .get('/search/photos', {
+        params: { query: term },
+
+      });
+    this.setState({ images: response.data.results });
+  }
+
   render() {
     return (
-      <div>
-        App is working
+      <div className="ui container style={{marginTop:'10px'}}">
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
